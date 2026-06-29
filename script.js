3436c5ae348a54f7dacaa862fbaccfc36f57977b
mohammadaffanr786-afk <mohammadaffanr786@gmail.com>
2026-06-29 17:28:20 +0530

    added online shoping

---
+++ script.js
@@ -0,0 +1,10 @@
+
+    function showSection(section) {
+        document.getElementById("home").style.display = "none";
+        document.getElementById("about").style.display = "none";
+        document.getElementById("contact").style.display = "none";
+        document.getElementById(section).style.display = "block";
+    }
+    function orderplaced() {
+        document.getElementById("thanks for shoping").innerHTML="order placed successfully!";  
+    }
